0cc2ec35c109d30e457260dd2c112e72f73da82b
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-09-19 22:14:01 +0300

    0x08. Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Making change interview question problem solution
+"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins
+       needed to meet a given amount total
+    """
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
